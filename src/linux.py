93cef82299030285d840919389a49de3e5aba2ee
Julian Imhof <julianimhof00@gmail.com>
2022-05-03 15:28:06 +0200

    feat: add new location for file scanning on linux

--- src/linux.py
+++ src/linux.py
@@ -2,7 +2,9 @@ import globals
 from pathlib import Path
 from os.path import expanduser
 
+# I'm happy to add more locations here, if they are reasonable.
 possibleLocations = [expanduser("~/Programs/TeamSpeak/html/client_ui/index.html"),\
+                     expanduser("~/.local/share/TeamSpeak/html/client_ui/index.html"),\
                      "/opt/TeamSpeak/html/client_ui/index.html"]
 
 def searchForInstallation():
@@ -10,7 +12,8 @@ def searchForInstallation():
         try:
             if Path(x).is_file():
                 return x
-        except: pass
+        except:
+            pass
     return ""
 
 def initPlatform():
